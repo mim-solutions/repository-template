9861aec372368ccb2d0123ef5c516a44ae1982fc
Marek Wachnicki <27623090+mwachnicki@users.noreply.github.com>
2024-03-22 16:49:41 +0100

    test

--- {{ cookiecutter.project_slug }}/.ci/stripped_notebooks_check.sh
+++ {{ cookiecutter.project_slug }}/.ci/stripped_notebooks_check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 TEMP=$(mktemp --directory)
-python .hooks/strip_notebooks.py --all --no-index --output "$TEMP"
+.hooks/strip_notebooks.py --all --no-index --output "$TEMP"
 diff -rq ./stripped "$TEMP" --exclude .gitkeep
 rm -rf "$TEMP"
